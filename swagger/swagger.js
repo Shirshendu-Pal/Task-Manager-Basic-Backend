ff870b3f23efbf3c3a09a3e3de3e1ebe8a2f2fdf
Shirshendu-Pal <shirshendupal9@gmail.com>
2025-01-31 04:06:29 +0530

    first

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -1,70 +1,69 @@
 const express = require('express');
-
 const router = express.Router();
-
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-
 const path = require('path');
 
-/* This is for Admin end swiagger API doc */
+/* This is for Admin end Swagger API doc */
 const optionsAdmin = {
-	swaggerDefinition: {
-		info: {
-			title: "basic-structure",
-			version: '1.0.0',
-			description: "basic-structure" + ' API Documentation',
-			contact: {
-				email: '',
-			},
-		},
-		tags: [
-			
-		],
-		schemes: ['http'],
-		host: 'localhost:8001',
-		// schemes: ['https','http',],
-		// host: "sentdrop.dedicateddevelopers.us",
-		basePath: '/',
-		securityDefinitions: {
-			Token: {
-				type: 'apiKey',
-				description: 'JWT authorization of an API',
-				// name: 'x-access-token',
-                name:"name",
-				in: 'header',
-			},
-		},
-	},
-
-	apis: [path.join(__dirname, '../modules/**/routes/*.routes.js')],
+  swaggerDefinition: {
+    info: {
+      title: "basic-structure",
+      version: '1.0.0',
+      description: "basic-structure" + ' API Documentation',
+      contact: {
+        email: '',
+      },
+    },
+    tags: [],
+    basePath: '/api',
+    securityDefinitions: {
+      Token: {
+        type: 'apiKey',
+        description: 'JWT authorization of an API',
+        name: 'token',
+        in: 'header',
+      },
+    },
+  },
+  apis: [path.join(__dirname, '../modules/**/routes/*.routes.js')],
 };
 
+// Set up Swagger dynamically based on the request's host
+router.use((req, res, next) => {
+  // Dynamically set the host based on the request's host (domain)
+  const host = req.get('host'); // Gets the domain (e.g., xyz.com or abc.com)
+  console.log("dynamic swagger host",host)
+  optionsAdmin.swaggerDefinition.host = host; // Update the host in Swagger options
 
+  if(host.includes("localhost")){
+	optionsAdmin.swaggerDefinition.schemes = ["http"]
+  }else{
+	optionsAdmin.swaggerDefinition.schemes = ["https", "http"]
+  }
 
-const swaggerSpec = swaggerJSDoc(optionsAdmin);
-require('swagger-model-validator')(swaggerSpec);
+  const swaggerSpec = swaggerJSDoc(optionsAdmin);
+  require('swagger-model-validator')(swaggerSpec);
 
-router.get('/apidoc-json', (req, res) => {
-	res.setHeader('Content-Type', 'application/json');
-	res.send(swaggerSpec);
+  // Serve the Swagger UI
+  router.get('/apidoc-json', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(swaggerSpec);
+  });
+
+  router.use('/apidoc', swaggerUi.serveFiles(swaggerSpec), swaggerUi.setup(swaggerSpec));
+
+  next(); // Continue with the request cycle
 });
 
-router.use('/apidoc', swaggerUi.serveFiles(swaggerSpec), swaggerUi.setup(swaggerSpec));
-
-
 function validateModel(name, model) {
-	const responseValidation = swaggerSpec.validateModel(name, model, false, true);
-	if (!responseValidation.valid) {
-		throw new Error('Model doesn\'t match Swagger contract');
-	}
+  const responseValidation = swaggerSpec.validateModel(name, model, false, true);
+  if (!responseValidation.valid) {
+    throw new Error('Model doesn\'t match Swagger contract');
+  }
 }
 
-
-
-
-
 module.exports = {
-	router,
-	validateModel
+  router,
+  validateModel,
 };
